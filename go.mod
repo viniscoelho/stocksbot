ae0b9a011723fcff4eac7bbe0bc04d459e1c5b67
Vinicius Coelho <vinicoelhose@gmail.com>
2020-06-29 23:25:33 -0300

    Add code for stocks bot
    This commit implements the stocks bot fetching information regarding currency.
    It is added a cronjob to run it every 5 minutes to fetch the content.
    The message will only be publish if it meets some thresholds defined on the code.
    Later, actual stock information will be added to the response.

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/stocksbot
+
+go 1.13
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/robfig/cron v1.2.0
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/sirupsen/logrus v1.6.0
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"strconv"
+
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api"
+	cron "github.com/robfig/cron/v3"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	EPS = 1e-9
+
+	telegramToken  = ""
+	telegramChatID = int64(0)
+
+	coinsAPI = "https://economia.awesomeapi.com.br/all/USD-BRL,EUR-BRL"
+)
+
+type CurrencyDTO struct {
+	Code      string `json:"code"`
+	CodeIn    string `json:"codein"`
+	Name      string `json:"name"`
+	PctChange string `json:"pctChange"`
+	Bid       string `json:"bid"`
+	Ask       string `json:"ask"`
+	Timestamp string `json:"create_date"`
+}
+
+type FloatComp int
+
+const (
+	Less FloatComp = iota
+	Equal
+	More
+)
+
+var (
+	USDLowThreshold  float64
+	USDHighThreshold float64
+	EURLowThreshold  float64
+	EURHighThreshold float64
+)
+
+func main() {
+	USDLowThreshold = 5.1
+	USDHighThreshold = 5.5
+
+	EURLowThreshold = 5.8
+	EURHighThreshold = 6.2
+
+	apiResponse := make(chan map[string]CurrencyDTO)
+
+	c := cron.New()
+	// At every 5th minute past every hour from 9 through 17
+	// on every day-of-week from Monday through Friday
+	c.AddFunc("*/5 9-17 * * 1-5", func() {
+		values, err := getCurrencyValues()
+		if err != nil {
+			logrus.Fatal(err)
+		}
+
+		publish, err := processCurrency(values)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+
+		if publish {
+			apiResponse <- values
+			logrus.Info(values)
+		}
+	})
+	c.Start()
+
+	for {
+		select {
+		case resp := <-apiResponse:
+			err := sendCurrencyOnTelegram(formatResponse(resp))
+			if err != nil {
+				logrus.Fatal(err)
+			}
+		}
+	}
+}
+
+func processCurrency(values map[string]CurrencyDTO) (bool, error) {
+	flag := false
+	for _, v := range values {
+		cur, err := strconv.ParseFloat(v.Bid, 64)
+		if err != nil {
+			return flag, err
+		}
+
+		switch v.Code {
+		case "USD":
+			if floatCompare(cur, USDLowThreshold) == Less {
+				USDLowThreshold = cur - 0.05
+				flag = true
+			} else if floatCompare(cur, USDHighThreshold) == More {
+				USDHighThreshold = cur + 0.05
+				flag = true
+			}
+		case "EUR":
+			if floatCompare(cur, EURLowThreshold) == Less {
+				EURLowThreshold = cur - 0.05
+				flag = true
+			} else if floatCompare(cur, EURHighThreshold) == More {
+				EURHighThreshold = cur + 0.05
+				flag = true
+			}
+		}
+	}
+
+	return flag, nil
+}
+
+func floatCompare(a, b float64) FloatComp {
+	if math.Abs(a-b) < EPS {
+		return Less
+	} else if math.Abs(a-b) > EPS {
+		return More
+	}
+	return Equal
+}
+
+func formatResponse(values map[string]CurrencyDTO) string {
+	fmtRes := "Cotações\n"
+	for _, v := range values {
+		fmtRes += fmt.Sprintf(`
+		Moeda: %s
+		Variação Cambial: %s
+		Venda: %s
+		Compra: %s
+		Horário: %s
+		`, v.Name, v.PctChange, v.Bid, v.Ask, v.Timestamp)
+	}
+	return fmtRes
+}
+
+func getCurrencyValues() (map[string]CurrencyDTO, error) {
+	resp, err := http.Get(coinsAPI)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch currencies: %s", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read body content: %s", err)
+	}
+	logrus.Infof("%s", string(body))
+
+	dto := make(map[string]CurrencyDTO)
+	err = json.Unmarshal(body, &dto)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse body content: %s", err)
+	}
+
+	return dto, nil
+}
+
+func sendCurrencyOnTelegram(content string) error {
+	botAPI, err := telegram.NewBotAPI(telegramToken)
+	if err != nil {
+		return err
+	}
+
+	msgConfig := telegram.NewMessage(telegramChatID, content)
+	_, err = botAPI.Send(msgConfig)
+	return err
+}
